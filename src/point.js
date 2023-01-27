f49a7e66d53c3725b1b428e3685b7d28b363942d
Daniel Roux <roux@epsitec.ch>
2023-01-27 12:59:31 +0100

    Add some JSDoc

--- src/point.js
+++ src/point.js
@@ -1,6 +1,12 @@
 import Misc from "./misc";
 
 export default class Point {
+  /**
+   * Constructor, make a Point.
+   * @param {number} x - coordinate X: 0..800 pixels
+   * @param {number} y - coordinate Y: 0..480 pixels, from top to bottom
+   * @returns {Point}
+   */
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -230,6 +236,13 @@ export default class Point {
 
   //------------------------------------------------------------------------
 
+  /**
+   * Rotates a point around a center.
+   * @param {Point} center - center of the circle
+   * @param {number} angle - angle in degrees -360..360
+   * @param {Point} p - point on the circle
+   * @returns {Point} p' rotated point
+   */
   static rotatePointDeg(center, angle, p) {
     return Point.rotatePointRad(center, Misc.degToRad(angle), p);
   }
@@ -263,7 +276,13 @@ export default class Point {
     return { x: a.x + (b.x - a.x) * scale, y: a.y + (b.y - a.y) * scale };
   }
 
-  //	Avance d'une certaine distance le long d'une droite AB.
+  /**
+   * Moves a certain distance along a line AB.
+   * @param {Point} a - A: origin
+   * @param {Point} b - B: point along the line
+   * @param {number} distance
+   * @returns {Point} point 'a' moved
+   */
   static move(a, b, distance) {
     const length = Point.distance(a, b);
     const scale = length === 0 ? 0 : distance / length;
